61b95270fa729a3fdf45578f20b4813aca804625
[zubair bhuian] <[zubairbhuian@gmail.com]>
2022-01-16 15:49:32 +0600

    up:1.16.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,30 @@ function showsDoc() {
 document.querySelector(".shows-btn").classList.toggle("btnBorder");
 document.querySelector(".shows-nav").classList.toggle("showf");
 };
+function getCodeBoxElement(index) {
+  return document.getElementById('codeBox' + index);
+}
+function onKeyUpEvent(index, event) {
+  const eventCode = event.which || event.keyCode;
+  if (getCodeBoxElement(index).value.length === 1) {
+    if (index !== 6) {
+      getCodeBoxElement(index+ 1).focus();
+    } else {
+      getCodeBoxElement(index).blur();
+      // Submit code
+      console.log('submit code ');
+    }
+  }
+  if (eventCode === 8 && index !== 1) {
+    getCodeBoxElement(index - 1).focus();
+  }
+}
+function onFocusEvent(index) {
+  for (item = 1; item < index; item++) {
+    const currentElement = getCodeBoxElement(item);
+    if (!currentElement.value) {
+        currentElement.focus();
+        break;
+    }
+  }
+} 
